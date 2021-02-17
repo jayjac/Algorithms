b6bae0213a9ad4c0d636de692cb1133da3907e7f
JayJac <jy_jac@msn.com>
2021-02-17 09:43:07 -0500

    165. Compare Version Numbers

---
+++ LeetCode/165. Compare Version Numbers/index.ts
@@ -0,0 +1,17 @@
+function compareVersion(version1: string, version2: string): number {
+  const arr1 = version1.split('.');
+  const arr2 = version2.split('.');
+  const maxLength = arr1.length > arr2.length ? arr1.length : arr2.length;
+
+  //console.log(maxLength)
+
+  for(let i = 0; i < maxLength; i++) {
+    const v1 = i < arr1.length ? Number(arr1[i]) : 0;
+    const v2 = i < arr2.length ? Number(arr2[i]) : 0;
+
+    //console.log("v1 = %d | v2 = %d", v1, v2)
+    if(v1 < v2) return -1;
+    if(v1 > v2) return 1;
+  }
+  return 0;
+};
