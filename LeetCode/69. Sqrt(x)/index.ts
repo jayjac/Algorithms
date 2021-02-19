190f00cfc453dad07998b8d034b4fe885d4ebbf3
JayJac <jy_jac@msn.com>
2021-02-18 22:10:33 -0500

    sqrt enhanced

--- LeetCode/69. Sqrt(x)/index.ts
+++ LeetCode/69. Sqrt(x)/index.ts
@@ -1,8 +1,21 @@
+// function mySqrt(x: number): number {
+//     if(x < 2) return x;
+//     const half = Math.round(x / 2);
+//     const start = x >= 100000000 ? 10000 : x >= 1000000 ? 1000 : x >= 10000 ? 100 : x >= 100 ? 10 : 1;
+//     for(let i = start; i <= half + 1; i++) {
+//         if(i * i > x) return i - 1;
+//     }
+// };
+
 function mySqrt(x: number): number {
     if(x < 2) return x;
-    const half = Math.round(x / 2);
-    const start = x >= 100000000 ? 10000 : x >= 1000000 ? 1000 : x >= 10000 ? 100 : x >= 100 ? 10 : 1;
-    for(let i = start; i <= half + 1; i++) {
-        if(i * i > x) return i - 1;
+    let half = Math.round(x / 2);
+    while(half * half > x) {
+        half = Math.round(half / 2);
     }
-};
+    if(half * half === x) return half;
+    while(half * half < x) {
+        half++;
+    }
+    return half * half === x ? half : half - 1;
+}
